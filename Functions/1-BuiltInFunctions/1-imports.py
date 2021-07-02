c5d3438796edc9de1c793730ebf0809693e9d82a
Isaac Basque-Rice <ibasquerice@gmail.com>
2021-07-02 15:14:39 +0100

    built in functions stuff

---
+++ Functions/1-BuiltInFunctions/1-imports.py
@@ -0,0 +1,5 @@
+# import random
+from random import *
+
+print(str(random.randint(1,10))) # accessing randint through the random class (import random)
+print(str(randint(1, 10))) # accessing randint through from random import *, bad practice
---
+++ Functions/1-BuiltInFunctions/3-pyperclip.py
@@ -0,0 +1,6 @@
+# 3rd party module for copy/pasting from clipboard
+# installed w/ pip3 install -U pyperclip
+import pyperclip
+
+pyperclip.copy('hello world')
+pyperclip.paste()

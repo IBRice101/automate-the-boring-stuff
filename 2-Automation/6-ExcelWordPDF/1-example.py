846587d9b1692927f05dcb6552712804ae868134
Isaac <ibasquerice@gmail.com>
2021-07-17 12:24:07 +0100

    added example word doc parser

---
+++ 2-Automation/6-ExcelWordPDF/1-example.py
@@ -0,0 +1,12 @@
+# returns all of the text from a word doc in plaintext format
+
+import docx
+
+def getText(filename):
+    doc = docx.Document(filename)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return '\n'.join(fullText)
+
+print(getText('Files/demo.docx'))
